e7e68c14038d93e9118c0b9ce443eadaf9a279c3
CodyS80 <bartolosemolo@gmail.com>
2025-10-01 12:18:54 +0200

    Dodanie projektu

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace INF04_2023_czerwiec_desktopApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace INF04_2023_czerwiec_desktopApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
