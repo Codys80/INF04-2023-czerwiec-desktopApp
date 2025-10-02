68e1415805d595dc0f735d3d830d88e510e49f6c
CodyS80 <bartolosemolo@gmail.com>
2025-10-02 11:16:33 +0200

    Update 1

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,83 @@
-﻿namespace INF04_2023_czerwiec_desktopApp
+﻿using System.Text.RegularExpressions;
+
+namespace INF04_2023_czerwiec_desktopApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        string source = " ";
+        double multiplyer = 1;
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        //****************************************************************
+        //Nazwa Funkcji:verifyEntryData
+        //Parametry wejściowe: obiekt wysyłający żądanie
+        //Wartość zwracana: brak
+        //Informacje: funkcja weryfikuje dane wprowadzane przez użytkownika
+        //Autor: Bartosz Semczuk
+        //****************************************************************
+        void verifyEntryData(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            displayError.Text = "W porządku";
+            if (postalCode.Text.Trim().All(char.IsLetter))
+            {
+                displayError.Text = "Kod pocztowy nie może zawierac liter";
+            }
+            if (displayError.Text.Length < 5)
+            {
+                displayError.Text = "Nieprwadiłowa liczba cyfr w kodzie pocztowym";
+            }
+            if (displayError.Text.Length > 5)
+            {
+                displayError.Text = "Nieprwadiłowa liczba cyfr w kodzie pocztowym";
+            }
         }
+        //****************************************************************
+        //Nazwa Funkcji:setPackageType
+        //Parametry wejściowe: obiekt wysyłający żądanie
+        //Wartość zwracana: brak
+        //Informacje: funkcja sprawdza i ustawia wybrany typ paczki
+        //Autor: Bartosz Semczuk
+        //****************************************************************
+        void setPackageType(object sender, EventArgs e)
+            {
+            RadioButton rb = sender as RadioButton;
+            switch (rb.Value)
+            {
+                case "1":
+                    {
+                        source = "list.png";
+                        multiplyer = 1;
+                        break;
+                    }
+                case "2":
+                    {
+                        source = "paczka.png";
+                        multiplyer = 1.5;
+                        break;
+                    }
+                case "3":
+                    {
+                        source = "pocztowka.png";
+                        multiplyer = 10;
+                        break;
+                    }
+            }
+        }
+        //****************************************************************
+        //Nazwa Funkcji:priceCheck
+        //Parametry wejściowe: obiekt wysyłający żądanie
+        //Wartość zwracana: brak
+        //Informacje: funkcja ustawia dane wyjściowe wyświetlane w graficznym interfejsie użytkownika
+        //Autor: Bartosz Semczuk
+        //****************************************************************
+        void priceCheck(object sender, EventArgs e) {
+            displayPrice.Text = "Cena: " + (1 * multiplyer)+ "zł";
+            displayPhoto.Source = source;
+        }
+
     }
 
 }
